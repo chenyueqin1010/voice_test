29d897791d4129b5455883c49ed44e990b1c1ada
andy <chneyueqin1010@sina.com>
2018-04-14 21:13:53 +0800

    文件上传

---
+++ base64string.js
@@ -0,0 +1,54 @@
+/**
+ * Created by fengxiaochuang on 2017/11/28.
+ */
+
+
+function bin2string(array) {
+    var result = "";
+    for (var i = 0; i < array.length; ++i) {
+        result += (String.fromCharCode(array[i] % 256) + String.fromCharCode(array[i] >> 8));
+    }
+    return result;
+}
+
+function base64encode(str) {
+    var base64EncodeChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
+    var out, i, len;
+    var c1, c2, c3;
+    len = str.length;
+    i = 0;
+    out = "";
+    while (i < len) {
+        c1 = str.charCodeAt(i++) & 0xff;
+        if (i == len) {
+            out += base64EncodeChars.charAt(c1 >> 2);
+            out += base64EncodeChars.charAt((c1 & 0x3) << 4);
+            out += "==";
+            break;
+        }
+        c2 = str.charCodeAt(i++);
+        if (i == len) {
+            out += base64EncodeChars.charAt(c1 >> 2);
+            out += base64EncodeChars.charAt(((c1 & 0x3) << 4) | ((c2 & 0xF0) >> 4));
+            out += base64EncodeChars.charAt((c2 & 0xF) << 2);
+            out += "=";
+            break;
+        }
+        c3 = str.charCodeAt(i++);
+        out += base64EncodeChars.charAt(c1 >> 2);
+        out += base64EncodeChars.charAt(((c1 & 0x3) << 4) | ((c2 & 0xF0) >> 4));
+        out += base64EncodeChars.charAt(((c2 & 0xF) << 2) | ((c3 & 0xC0) >> 6));
+        out += base64EncodeChars.charAt(c3 & 0x3F);
+    }
+    return out;
+}
+
+function _arrayBufferToBase64(buffer) {
+    var binary = '';
+    var bytes = new Uint8Array(buffer);
+    var len = bytes.byteLength;
+    for (var i = 0; i < len; i++) {
+        binary += String.fromCharCode(bytes[i]);
+    }
+    return window.btoa(binary);
+}
---
+++ xunfei.js
@@ -0,0 +1,97 @@
+$(function(){
+
+	$('#startRecord').click(function(){
+		 start_recorder(function (state) {
+			if (state == true) {
+				console.log('录音中……');
+			}
+		});
+	});
+	$('#stopRecord').click(function(){
+		stop_recorder();
+		
+		var audioData = get_recorder_data();
+		var apiKey = "2d54f7bcbb2d41fbba1bbf6dfb391718";
+		var time = new Date().getTime();
+		var x_param = "eyJlbmdpbmVfdHlwZSI6ICJzbXMxNmsiLCJhdWUiOiAicmF3In0";
+		
+		$.ajax({
+			 headers: {
+				'X-Appid': "5ad05bef",
+                'X-CurTime': time,
+                'X-Param': x_param,
+                'X-CheckSum': md5(apiKey+time+x_param)
+			},
+			url: "http://api.xfyun.cn/v1/service/v1/iat",
+			data: {
+				"audio": audioData
+			},
+			type: "post",
+			dataType: "jsonp",
+			beforeSend: function () {
+				console.log("语音识别中……");
+			},
+			success: function (data) {
+				console.log(data);
+				/* if (data.status == 1) {
+					console.log(data.info);
+				} else {
+					console.error(data.info);
+				} */
+			},
+			error: function (res) {
+				console.log(res);
+			},
+			complete: function () {
+				//console.info("识别完毕！");
+			}
+		});
+	});
+	
+});
+
+var recorder;
+//IE浏览器判断
+function isIE(){
+	var ua = window.navigator.userAgent;
+	var msie = ua.indexOf("MSIE ");
+	return !!(msie > 0 || navigator.userAgent.match(/Trident.*rv:11./));
+}
+//开始录音
+function start_recorder(callback) {
+	if (isIE()) {
+		FWRecorder.configure(16, 100, 0, 0);
+		FWRecorder.record('flash-asr', 'flash-asr.wav');
+		callback(true);
+	} else {
+		HZRecorder.get(function (error, rec) {
+			if (error) {
+				layer.msg(error.message, {icon: 5});
+				callback(false);
+				return false;
+			}
+			recorder = rec;
+			recorder.start();
+			callback(true);
+		}, {
+			sampleBits: 16,
+			sampleRate: 16000
+		});
+	}
+}
+//获取录音数据
+function get_recorder_data() {
+	if (isIE()) {
+		return FWRecorder.getBase64('flash-asr');
+	} else {
+		return recorder.getBase64AudioData();
+	}
+}
+//结束录音
+function stop_recorder() {
+	if (isIE()) {
+		FWRecorder.stopRecording('flash-asr');
+	} else {
+		recorder.stop();
+	}
+}
